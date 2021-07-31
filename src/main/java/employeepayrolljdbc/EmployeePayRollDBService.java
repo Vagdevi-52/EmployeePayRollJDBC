6a03d627d0c5d335c9603d293eaa87fa538cb386
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-07-31 07:20:34 -0700

    Added: Program To Create A PayRoll Service DataBase

--- src/main/java/employeepayrolljdbc/EmployeePayRollDBService.java
+++ src/main/java/employeepayrolljdbc/EmployeePayRollDBService.java
@@ -1,9 +1,45 @@
 package employeepayrolljdbc;
 
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
+
 public class EmployeePayRollDBService
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome To Employee PayRoll JDBC Program");
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+		} 
+		catch (ClassNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		ListDrivers();
+		try
+		{
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll_service?useSSL=false"
+				, "root", "Peddamavaya_1952@");
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+		System.out.println("Connection Done..!!!");
+	}
+
+	private static void ListDrivers()
+	{
+		Enumeration<Driver> driverList=DriverManager.getDrivers();
+		while(driverList.hasMoreElements())
+		{
+			Driver driverClass=(Driver)driverList.nextElement();
+			System.out.println("  "+driverClass.getClass().getName());
+		}
 	}
 }
