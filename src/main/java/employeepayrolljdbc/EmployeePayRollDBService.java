24a6930d6fe26236d8e09b30632873211ae3c728
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-07-31 07:14:48 -0700

    Initial Commit: Welcome To Employee PayRoll JDBC Program

---
+++ src/main/java/employeepayrolljdbc/EmployeePayRollDBService.java
@@ -0,0 +1,9 @@
+package employeepayrolljdbc;
+
+public class EmployeePayRollDBService
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome To Employee PayRoll JDBC Program");
+	}
+}
